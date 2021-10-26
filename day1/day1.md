3a56be99f38cd578055cdbf815c695ddccbe18c8
dearshrewdwit <dearshrewdwit@gmail.com>
2021-10-26 10:43:47 +0100

    Day one

---
+++ day1/day1.md
@@ -0,0 +1,12 @@
+## Learning Objectives
+
+- Explain how the browser runs JavaScript
+  - the difference between the file and the browser console
+- Declare and initialize variables using let and const
+- Explain the purpose of a variable
+- Explain how to create object literals with properties and values
+
+## Evidence
+- Written/verbal explanation of (1), or a diagram
+- A program that uses variables with both keywords (let and const)
+- Written/verbal explanation of (3)
---
+++ day1/index.js
@@ -0,0 +1,32 @@
+// calculate the average of letters in the names
+const names = ["Yancy", "Nico", "Ed"]
+// average = total/number
+let total = 0
+for (let i = 0; i < names.length; i++) {
+  const name = names[i]
+  const nameLength = name.length
+  total = total + nameLength
+}
+
+
+
+
+// console.log("Hi, Yancy and Nico")
+//
+// // variable declarations
+// // const - modifier ->
+// const age = 5 // can not change the value
+// let name // can change the value
+//
+// // variable initializations
+// name = ""
+//
+//
+// for (const i = 0; i < 10; i++) {
+//   console.log(i)
+//   // do some stuff
+// }
+//
+// const variable1 = {}
+// const variable2 = []
+// const variable3 = variable2
